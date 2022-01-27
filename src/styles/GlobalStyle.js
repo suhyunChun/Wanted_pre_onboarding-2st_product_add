1a6ff0924379873d607f2345535895e1f69fcb27
suhyun <suhyun.chun@stonybrook.edu>
2022-01-28 02:35:25 +0900

    FIX: change css style to styled component

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,6 +7,8 @@ const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
   }
 
+  
+
 `;
 
 export default GlobalStyle;
---
+++ src/utils/UploadImageFile.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const UploadImageFile = ({ title, imgArray }) => {
+  const [tmpArr, setTmpArr] = useState(imgArray);
+
+  const Uploadimagecontainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    width: 800px;
+    flex-direction: column;
+    margin: 30px;
+  `;
+  const UploadImageTitle = styled.div`
+    border-bottom: 1px solid lightgray;
+    width: 100%;
+    float: left;
+    display: flex;
+    padding: 10px;
+    font-weight: 700;
+  `;
+  const UploadImageInfo = styled.div`
+    margin: 10px;
+    display: flex;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    flex-direction: row;
+  `;
+  const UploadImageBtn = styled.label`
+    font-size: 16px;
+    padding: 10px;
+    border: 2px solid #1c1c81;
+    width: 130px;
+    height: 45px;
+
+    font-weight: 550;
+  `;
+  const ImageList = styled.div`
+    margin-left: 5%;
+  `;
+  const CancleBtn = styled.button`
+    border-radius: 100%;
+    border: 1px solid lightgray;
+    background-color: transparent;
+  `;
+  return (
+    <Uploadimagecontainer>
+      <UploadImageTitle>{title}</UploadImageTitle>
+      <UploadImageInfo>
+        <UploadImageBtn>
+          <input
+            id="input-file-img"
+            style={{ width: '0%' }}
+            type="file"
+            accept="image/*"
+            onChange={e => setTmpArr([e.target.files[0].name].concat(tmpArr))}
+          />
+          + 이미지 추가
+        </UploadImageBtn>
+        <ImageList>
+          {tmpArr.map(i => (
+            <div
+              onClick={e => {
+                setTmpArr(tmpArr.filter(e => e !== i));
+              }}
+              style={{
+                fontSize: '16px',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                marginBottom: '10px',
+              }}
+            >
+              {i} <CancleBtn> X </CancleBtn>
+            </div>
+          ))}
+        </ImageList>
+      </UploadImageInfo>
+    </Uploadimagecontainer>
+  );
+};
+
+export default UploadImageFile;

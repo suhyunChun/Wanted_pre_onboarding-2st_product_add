0863ba5d700713f8d1de70c86f1933316d7c65dc
suhyun <suhyun.chun@stonybrook.edu>
2022-01-28 03:06:14 +0900

    Add: add ProductBenefit.js

--- src/pages/AddProductInfo/SetProductBenefit.js
+++ src/pages/AddProductInfo/SetProductBenefit.js
@@ -1,20 +1,35 @@
 import React, { useState } from 'react';
 import ToggleButton from 'react-toggle-button';
+import styled from 'styled-components';
 
 const SetProductBenefit = () => {
   const [productBenefit, setProductBenefit] = useState(true);
+
+  const BenefitConatiner = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 800px;
+    height: 60px;
+  `;
+  const BenefitTitle = styled.div`
+    background-color: #e4e4e4;
+    display: flex;
+    width: 150px;
+    height: 100%;
+    align-items: center;
+    margin-right: 40px;
+  `;
+
   return (
-    <div>
-      <div>마일리지 적립</div>
-      <div>
-        <ToggleButton
-          value={productBenefit}
-          onToggle={value => {
-            setProductBenefit(e => !e);
-          }}
-        />
-      </div>
-    </div>
+    <BenefitConatiner>
+      <BenefitTitle>마일리지 적립</BenefitTitle>
+      <ToggleButton
+        value={productBenefit}
+        onToggle={value => {
+          setProductBenefit(e => !e);
+        }}
+      />
+    </BenefitConatiner>
   );
 };
 

971070515dc10b2bda021689499b785b97487aa7
choi2601 <byeonghyeonchoe55@gmail.com>
2022-01-29 06:32:17 +0900

    Add: SetProductStore feature add

--- src/pages/AddProductInfo/SetDefaultProductInfo/SetProductStore.js
+++ src/pages/AddProductInfo/SetDefaultProductInfo/SetProductStore.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const SetProductStore = () => {
-  return <div>SetProductStore</div>;
+const SetProductStore = ({ currentOptionNum }) => {
+  return (
+    <Container>
+      <p className="currentProductStoreNum">20개</p>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  margin-top: 15px;
+  margin-left: 15px;
+
+  & .currentProductStoreNum {
+    font-weight: 600;
+  }
+`;
 
 export default SetProductStore;

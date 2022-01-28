b1b88db746e17dba6f6b864b96804f2874a35364
Mcdonalds Trump <whdmschd2@gmail.com>
2022-01-28 17:46:50 +0900

    Add: option 레이아웃 추가

--- src/pages/AddProductInfo/SetProductOption/OptionSet.js
+++ src/pages/AddProductInfo/SetProductOption/OptionSet.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import Option from './Option';
 
-const OptionSet = ({ index, key, arrayOptionSet, setArrayOptionSet }) => {
+const OptionSet = ({ id, arrayOptionSet, setArrayOptionSet }) => {
   const clickRemoveOptionSet = () => {
-    console.log(arrayOptionSet);
-    console.log(index);
-    const copy = arrayOptionSet.filter((optionset, i) => i !== index);
-    console.log(copy);
-    // setArrayOptionSet(arrayOptionSet.filter((optionset, i) => i !== index));
+    console.log(id);
+    setArrayOptionSet(arrayOptionSet.filter(a => a !== id));
   };
   return (
     <section>
       <h1>옵션세트추가 성공</h1>
       <button onClick={clickRemoveOptionSet}>옵션세트 삭제</button>
-      {index}
+      {id}
+      <OptionSetContainer>
+        <input type="file" accept="image/*" />
+        <Option />
+        <button>옵션추가</button>
+      </OptionSetContainer>
     </section>
   );
 };
 
+const OptionSetContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
 export default OptionSet;
